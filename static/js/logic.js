164cc5c422d5d85f1accafb7b27ce6ce89fc3847
Marsha Curtis <curt0230@gmail.com>
2022-06-16 08:41:43 -0500

    added data file for module 13.5.3

--- static/js/logic.js
+++ static/js/logic.js
@@ -12,33 +12,71 @@ console.log("working");
 //     fillColor: "#ffffa1"
 // }).addTo(map);
 
-//13.4.3
+// //13.4.3
+// // Create the map object with center at the San Francisco airport.
+// let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+
+// // Coordinates for each point to be used in the polyline.
+// let line = [
+//     // [33.9416, -118.4085], LAX
+//     [37.6213, -122.3790], //san fran
+//     // [40.7899, -111.9791], //salt lake
+//     // [47.4502, -122.3088] //seattle
+//     [30.1975, -97.6664], //austin
+//     [43.6777, -79.6248], //toronto
+//     [40.6413, -73.7781] //jfk
+//   ];
+
+
+// // Create a polyline using the line coordinates and make the line red.
+// L.polyline(line, {
+//     color: "blue",
+//     dashArray: '20, 20', 
+//     dashOffset: '20',
+//   }).addTo(map);
+
+//13.5.1
 // Create the map object with center at the San Francisco airport.
-let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+let map = L.map('mapid').setView([37.61899948120117, -122.375], 12.5); // Coordinates are lat, long
 
-// Coordinates for each point to be used in the polyline.
-let line = [
-    // [33.9416, -118.4085], LAX
-    [37.6213, -122.3790], //san fran
-    // [40.7899, -111.9791], //salt lake
-    // [47.4502, -122.3088] //seattle
-    [30.1975, -97.6664], //austin
-    [43.6777, -79.6248], //toronto
-    [40.6413, -73.7781] //jfk
-  ];
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}} // coordinates are long/lat
+]};
 
-// Create a polyline using the line coordinates and make the line red.
-L.polyline(line, {
-    color: "blue",
-    dashArray: '20, 20', 
-    dashOffset: '20',
+// Grabbing our GeoJSON data.
+L.geoJSON(sanFranAirport, {
+    // We turn each feature into a marker on the map.
+    // pointToLayer: function(feature, latlng) {
+    //   console.log(feature);
+    //   return L.marker(latlng)
+    //   .bindPopup("<h3>" + feature.properties.name + "</h3><h4>" + feature.properties.city + ', ' + feature.properties.country + "</h4>");
+    onEachFeature: function(feature,layer){
+        console.log(layer);
+        layer.bindPopup("<h3>" + feature.properties.name + "</h3><h4>" + feature.properties.city + ', ' + feature.properties.country + "</h4>");
+    }
   }).addTo(map);
-
+  
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-night-v1/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'satellite-streets-v11',
+    id: 'navigation-night-v1',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: API_KEY
